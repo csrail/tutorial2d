08c3057d1fc9f9eec72241719bec81497f930fdf
Cornelius Soon <cornelius.soon@gmail.com>
2024-03-03 13:30:50 +1300

    Initialise project

---
+++ Player.cs
@@ -0,0 +1,93 @@
+using Godot;
+using System;
+
+public partial class Player : Area2D
+{
+	public void Start(Vector2 position)
+	{
+		Position = position;
+		Show();
+		GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
+	}
+	
+	[Signal]
+	public delegate void HitEventHandler();
+
+	private void OnBodyEntered(Node2D body)
+	{
+		Hide(); // Player disappears after being hit.
+		EmitSignal(SignalName.Hit);
+		GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
+	}
+	
+	[Export]
+	public int Speed { get; set; } = 400; // How fast the player will move (pixels/sec)
+	public Vector2 ScreenSize; // Size of the game window
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		ScreenSize = GetViewportRect().Size;
+		//Hide();
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+
+		// Assigns magnitude to Player's velocity.
+		var velocity = Vector2.Zero;
+
+		if (Input.IsActionPressed("move_right"))
+		{
+			velocity.X += 1;
+		}
+
+		if (Input.IsActionPressed("move_left"))
+		{
+			velocity.X -= 1;
+		}
+
+		if (Input.IsActionPressed("move_down"))
+		{
+			velocity.Y += 1;
+		}
+
+		if (Input.IsActionPressed("move_up"))
+		{
+			velocity.Y -= 1;
+		}
+
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+		if (velocity.Length() > 0)
+		{
+			// Prevents diagonal movement from being faster.
+			velocity = velocity.Normalized() * Speed;
+			animatedSprite2D.Play();
+		}
+		else
+		{
+			animatedSprite2D.Stop();
+		}
+
+		// Prevents Player from leaving the canvas.
+		Position += velocity * (float) delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+		);
+
+		if (velocity.X != 0)
+		{
+			animatedSprite2D.Animation = "walk";
+			animatedSprite2D.FlipV = false;
+			animatedSprite2D.FlipH = velocity.X < 0;
+		}
+		else if (velocity.Y != 0)
+		{
+			animatedSprite2D.Animation = "up";
+			animatedSprite2D.FlipV = velocity.Y > 0;
+		}
+	}
+}
